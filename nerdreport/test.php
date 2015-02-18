eef04a0f8880ca1024b2490438f26674be160748
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-02-18 11:59:26 -0800

    setting up d3 visulization

--- nerdreport/test.php
+++ nerdreport/test.php
@@ -7,11 +7,18 @@
   <title>My Nerd Report</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="styles.css?v=1.0">
 </head>
 <body>
   <h1>My Nerd Report</h1>
+  <script>
+    d3.select("body")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
+  </script>
   <p>&copy; Andrea Moulding <?php echo date('m/d/Y');?></p>
 </body>
 </html>
