128beb40c9377751e5d1dcd53036b4fe117ec0a8
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-02-19 16:26:10 -0800

    hosting from local drive

--- nerdreport/test.php
+++ nerdreport/test.php
@@ -7,16 +7,18 @@
   <title>My Nerd Report</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
-  <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+  <script type="text/javascript" src="d3/d3.js"></script>
+  <script type="text/javascript" src="d3/d3.geom.js"></script>
+  <script type="text/javascript" src="d3/d3.layout.js"></script>
   <link rel="stylesheet" href="styles.css?v=1.0">
 </head>
 <body>
   <h1>My Nerd Report</h1>
   <script>
-    d3.select("body")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
+    var canvas = d3.select("body")
+                .append("svg")
+                .attr("width", 500)
+                .attr("height", 500);
 
   </script>
   <p>&copy; Andrea Moulding <?php echo date('m/d/Y');?></p>
