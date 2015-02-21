a5d24f48a302efa035ea46eae8f9050ca93772db
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-02-20 19:03:40 -0800

    d3 methods

--- nerdreport/test.php
+++ nerdreport/test.php
@@ -8,15 +8,15 @@
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
   <script type="text/javascript" src="d3/d3.js"></script>
-  <script type="text/javascript" src="d3/d3.geom.js"></script>
-  <script type="text/javascript" src="d3/d3.layout.js"></script>
   <link rel="stylesheet" href="styles.css?v=1.0">
 </head>
 <body>
   <h1>My Nerd Report</h1>
-  <script>
+  <script type="text/javascript">
     var canvas = d3.select("body")
                 .append("svg")
+                .style("color", "black")
+                .style("background-color", "black")
                 .attr("width", 500)
                 .attr("height", 500);
 
